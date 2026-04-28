a444d8440dd4a6c983d4b5f6b94ea7a2a6cda91b
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2026-04-28 20:16:26 +0530

    added user Details

--- frontend/src/components/UserDetails.jsx
+++ frontend/src/components/UserDetails.jsx
@@ -1,19 +1,75 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+
 
 const UserDetails = () => {
-  return (
-    <div className="mt-40 pl-4 pr-4 flex justify-center text-center text-xl font-semibold text-white">
-        
-        <div className="max-w-3xl w-full bg-gray-900 p-6 rounded-3xl shadow-lg">
-            <h2 className="text-2xl font-bold mb-4">User Details</h2>
-            
-            <div className="space-y-4">
-                <p><span className="font-semibold">Name:</span> John Doe</p>
-                <p><span className="font-semibold">Email:</span> john.doe@example.com</p>
+
+    const { backendUrl, token, setToken, userData, setUserData, loadUserProfileData, } = useContext(AppContext);
+    const [isEdit, setIsEdit] = useState(false);
+    const [image, setImage] = useState(false);
+
+    const updateUserProfileData = async () => {
+        try {
+            const formData = new FormData();
+            formData.append("name", userData.name);
+            formData.append("phone", userData.phone);
+            formData.append("gender", userData.gender);
+            formData.append("dob", userData.dob);
+            formData.append('address', userData.address);
+
+            if (image) {
+                formData.append("image", image);
+            }
+
+            const { data } = await axios.post(
+                `${backendUrl}/api/user/update-profile`,
+                formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+            );
+            if (data.success) {
+                toast.success(data.message);
+                loadUserProfileData();
+                setIsEdit(false);
+                setImage(false);
+            }
+        } catch (error) {
+            toast.error(data.message);
+        }
+    }
+
+    return (
+        <div className=" h-screen mt-32 min-h-screen w-full bg-linear-to-br flex items-center justify-center px-4 overflow-hidden">
+
+            <div className="max-w-3xl w-full bg-gray-900 p-6 rounded-3xl shadow-lg">
+                <h2 className="text-2xl text-yellow-300 font-bold mb-4">User Details</h2>
+
+                {
+                    isEdit ?
+                        <label htmlFor="image">
+                            <div className="inline-block  relative cursor-pointer">
+                                <img className="w-32 h-32 cursor-pointer rounded-full opacity-80" src={image ? URL.createObjectURL(image) : userData?.image || null} alt={userData?.name} />
+                                <img className="w-28 h-28 cursor-pointer rounded-full absolute bottom-6 right-8" src={!image ? 'https://th.bing.com/th/id/OIP.Bl6dInu-pv4nnfv-QAxgSwHaHa' : null} alt="edit" />
+                            </div>
+                        </label>
+                        : <img className="w-36 rounded" src={userData?.image || null} alt={userData?.name} />
+                }
+
+                {
+                    isEdit ? (
+                        
+                    ) : ()
+                    
+                }
+
             </div>
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default UserDetails
